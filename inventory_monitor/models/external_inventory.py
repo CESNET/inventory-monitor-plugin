f744571b0a24c0e52ca2e33143378f37375ef597
Jan Krupa <jan.krupa@cesnet.cz>
2025-11-07 08:15:06 +0000

    Removed our namings from external_inventory forms

--- inventory_monitor/models/external_inventory.py
+++ inventory_monitor/models/external_inventory.py
@@ -16,7 +16,7 @@ class ExternalInventory(NetBoxModel):
     external_id = models.CharField(
         unique=True,
         verbose_name="External System ID",
-        help_text="Unique identifier for the item in external inventory system (ID)",
+        help_text="Unique identifier from external inventory system",
         # TODO: after Migration and setting up the external_id, make this field non nullable and blankable
         null=True,
         blank=True,
@@ -25,85 +25,85 @@ class ExternalInventory(NetBoxModel):
 
     inventory_number = models.CharField(
         max_length=64,
-        verbose_name="Inventory Number (Asset Number)",
-        help_text="External asset identifier (INVENT_CIS)",
+        verbose_name="Inventory Number",
+        help_text="Asset number from external inventory system",
     )
-    name = models.CharField(max_length=255, verbose_name="Name", help_text="Item name/description (NAZEV)")
+    name = models.CharField(max_length=255, verbose_name="Name", help_text="Item name or description")
     serial_number = models.CharField(
         max_length=255,
         blank=True,
         null=True,
         verbose_name="Serial Number",
-        help_text="Serial or production number (CVYR)",
+        help_text="Serial or production number",
     )
     person_id = models.CharField(
         max_length=64,
         blank=True,
         null=True,
         verbose_name="Person ID",
-        help_text="ID of the person responsible for this item (OSOBA_OSCISLO)",
+        help_text="ID of the person responsible for this item",
     )
     person_name = models.CharField(
         max_length=255,
         blank=True,
         null=True,
         verbose_name="Person Name",
-        help_text="Name of the person responsible for this item (OSOBA)",
+        help_text="Name of the person responsible for this item",
     )
     location_code = models.CharField(
         max_length=64,
         blank=True,
         null=True,
         verbose_name="Location Code",
-        help_text="Code representing the location of this item (KOD_UMISTENI)",
+        help_text="Code representing the location of this item",
     )
     location = models.CharField(
         max_length=255,
         blank=True,
         null=True,
         verbose_name="Location",
-        help_text="Description of item location (UMISTENI)",
+        help_text="Description of item location",
     )
     department_code = models.CharField(
         max_length=64,
         blank=True,
         null=True,
         verbose_name="Department Code",
-        help_text="Department code (AKTIVITA)",
+        help_text="Department code",
     )
     project_code = models.CharField(
         max_length=64,
         blank=True,
         null=True,
         verbose_name="Project Code",
-        help_text="Project code (PROJEKT)",
+        help_text="Project code",
     )
     user_name = models.CharField(
         max_length=255,
         blank=True,
         null=True,
         verbose_name="User Name",
-        help_text="Name of the user (NAZEV_UZIV)",
+        help_text="Name of the user",
     )
     user_note = models.TextField(
         blank=True,
         null=True,
         verbose_name="User Notes",
-        help_text="Notes from the user (POZN_UZIV)",
+        help_text="Notes from the user",
     )
     split_asset = models.CharField(
         # max_length=1,
         blank=True,
         null=True,
         verbose_name="Split Asset",
-        help_text="Whether this is a split/shared asset (DELENY_MAJETEK): A=Yes, N=No",
+        help_text="Whether this is a split/shared asset",
     )
     status = models.CharField(
         # max_length=1,
         blank=True,
         null=True,
         verbose_name="Status",
-        help_text="Current status of the item (STAV): 1=Active, 0=Inactive",
+        help_text="Current status of the item",
     )
 
     ############## EXTRA FIELDS
